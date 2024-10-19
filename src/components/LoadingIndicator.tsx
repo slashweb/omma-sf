9e05a8f6d0ee9beac404637ed1eeed2e23b7f12a
Victor Lopez <victor@ommacash.com>
2024-10-19 13:06:18 -0700

    adding new gif to loading animation

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -5,10 +5,14 @@ export default function LoadingIndicator() {
 
     return (
         <>
-            <div className="flex justify-center items-center h-96">
-                <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-pink-300"></div>
+            <div className="flex justify-center items-center h-64">
+
             </div>
-            <div className="flex px-12 justify-center items-center text-center w-full mt-6">
+            <div className="flex justify-center items-center">
+                <div className="absolute -mt-32 -ml-8 animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-pink-300"></div>
+                <img src="/img/chocolate.gif" className={'z-50'} alt=""/>
+            </div>
+            <div className="flex px-12 justify-center items-center text-center w-full mt-10">
                 <h1 className="text-3xl font-bold text-pink-300 animate-pulse">
                     {t.loading}
                 </h1>
